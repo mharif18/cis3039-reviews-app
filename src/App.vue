06ee4ccc0b41fd017259421a163aa9241850663c
Tyrone Davison <t.p.davison@tees.ac.uk>
2025-10-18 15:14:34 +0000

    Add initial personalisation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Hello World</h1>
-  <p>
-    Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
-    read the documentation
-  </p>
+  <h1>Reviews</h1>
 </template>
 
 <style scoped></style>
